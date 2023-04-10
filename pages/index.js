81c470fc6a38f467721b687841c913faf1ade49f
emrecoban <fbdarktole@gmail.com>
2023-04-10 22:46:40 +0300

    Updated date component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from '../lib/posts';
+import Date from "../components/date";
 
 export async function getStaticProps(){
   const allPostsData = getSortedPostsData();
@@ -30,7 +31,7 @@ export default function Home({ allPostsData }) {
               <br />
               {id}
               <br />
-              {date}
+              <Date dateString={date} />
             </li>
           ))}
         </ul>
