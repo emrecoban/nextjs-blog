e5916b93f29fe205a6e16bc92e1bb30a47cbcc85
emrecoban <fbdarktole@gmail.com>
2023-04-10 21:42:16 +0300

    Implement getStaticProps to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
