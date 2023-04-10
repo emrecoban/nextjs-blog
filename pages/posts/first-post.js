e31edb660b7c6360f4c41343c44e81f222cf5b59
emrecoban <fbdarktole@gmail.com>
2023-04-10 19:53:23 +0300

    Added assets and metadata

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
+
 
 export default function FirstPost(){
     return (
         <>
+            <Head>
+                <title>First Post in Next.js</title>
+            </Head>
             <h1>It's first post!</h1>
             <h2>
                 <Link href="/">Back to home</Link>
