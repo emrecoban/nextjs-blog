fda20bebfb52ba81ecb8da6c698fcb0ee92c2987
emrecoban <fbdarktole@gmail.com>
2023-04-11 17:18:26 +0300

    Updated API route.

---
+++ pages/api/posts.js
@@ -0,0 +1,5 @@
+import { getSortedPostsData } from "../../lib/posts";
+// req = HTTP incoming message, res = HTTP server response
+export default function handler(req, res){
+    res.status(200).json(getSortedPostsData());
+}
