922b113a6a61125b548c58b027fd0cae6e951e45
emrecoban <fbdarktole@gmail.com>
2023-05-26 00:39:40 +0300

    Imported date component

--- pages/SearchComponent.js
+++ pages/SearchComponent.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import utilStyles from "../styles/utils.module.css";
 import Link from 'next/link';
+import Date from "../components/date";
 
 export default function SearchComponent({ data }) {
     const [posts, setPosts] = useState(data)
